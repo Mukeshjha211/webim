1c6520d4873fcb4e27b23ddab473587a208d0971
MukeshJha2111 <mukesh.jha@xemailverify.com>
2024-09-07 10:54:03 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with mukesh
+          Learn React with mukesh go with
         </a>
       </header>
     </div>
